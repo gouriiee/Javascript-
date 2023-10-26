12dd36e88572cda44299f2fe6a6d7f34621b2765
gouriiee <109792940+gouriiee@users.noreply.github.com>
2023-10-26 11:41:14 +0530

    Initial commit

---
+++ 10/script.js
@@ -0,0 +1,16 @@
+
+
+const number = process.argv[2];
+
+if (!number || isNaN(number) ) 
+{
+  console.log("Please provide a valid positive number as an argument.");
+} 
+else 
+{
+  console.log(`Multiplication Table for ${number}:\n`);
+  for (let i = 1; i <= 10; i++) 
+  {
+    console.log(`${number} x ${i} = ${number * i}`);
+  }
+}
